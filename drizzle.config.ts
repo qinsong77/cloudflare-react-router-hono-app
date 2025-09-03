228b4309e75f8a829e53f78acf057a839a51b784
notend <xyqinsong@163.com>
2025-09-03 20:15:47 +0800

    chore: remove CLOUDFLARE env

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,12 +2,7 @@ import type { Config } from 'drizzle-kit'
 
 export default {
   out: './migration/sqlite',
-  schema: './server/d1/schema/**.sql.ts',
+  schema: './server/d1/**.sql.ts',
   dialect: 'sqlite',
   driver: 'd1-http',
-  dbCredentials: {
-    databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
-    accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
-    token: process.env.CLOUDFLARE_TOKEN!,
-  },
 } satisfies Config
